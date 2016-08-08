e42524ea9bc5c742e96e733a21fc914cfa9d12e8
Glenn Rempe <glenn@rempe.us>
2016-08-08 14:50:49 -0700

    url not uri

--- config/sidekiq_environment.rb
+++ config/sidekiq_environment.rb
@@ -34,11 +34,11 @@ if ENV['TIERION_ENABLED'] && $blockchain.blank?
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { uri: redis_uri, namespace: 'sidekiq' }
+  config.redis = { url: redis_uri, namespace: 'sidekiq' }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { uri: redis_uri, namespace: 'sidekiq' }
+  config.redis = { url: redis_uri, namespace: 'sidekiq' }
   config.on(:startup) do
     schedule = YAML.load_file(File.expand_path('../../config/sidekiq_scheduler.yml', __FILE__))
     Sidekiq.schedule = schedule
